5ca4483e509adc74d5bde4e519a8ba8128030df3
MarinaDVetrova <33320338+MarinaDVetrova@users.noreply.github.com>
2020-07-14 23:27:09 +0300

    js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,1 @@
-function coinrate(valNum) {
-  if (converter.usd.value <= 0)
-  window.alert ('');
-    else {
-      document.getElementById('euro').value = (valNum/23);
-      document.getElementById('btc').value = (valNum/53);
-    }
-  }
+1
