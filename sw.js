9f138ed3e8673c09236b74de12cd30fd2ca7a678
MarinaDVetrova <33320338+MarinaDVetrova@users.noreply.github.com>
2021-04-16 15:17:40 +0300

    CR

--- sw.js
+++ sw.js
@@ -14,26 +14,27 @@ const staticAssets = [
     'images/no-image.jpg'
 ];
 
-//install event
-self.addEventListener('install', evt => {
-  evt.waitUntil(
-    caches.open(staticCacheName).then( cache => {
-      console.log('caching cell assets');
-        cache.addAll(assets)
-    })
-  )
+self.addEventListener('install', async event => {
+    const cache = await caches.open(staticCacheName);
+    await cache.addAll(staticAssets);
+    console.log('Service worker has been installed');
+});
 
-})
+self.addEventListener('activate', async event => {
+    const cachesKeys = await caches.keys();
+    const checkKeys = cachesKeys.map(async key => {
+        if (![staticCacheName, dynamicCacheName].includes(key)) {
+            await caches.delete(key);
+        }
+    });
+    await Promise.all(checkKeys);
+    console.log('Service worker has been activated');
+});
 
-//activate event
-self.addEventListener('activate', evt => {
-  console.log('service worker activated');
-})
-
-//fetch event
-self.addEventListener('fetch', evt => {
-  console.log('fetch Event', evt);
-})
+self.addEventListener('fetch', event => {
+    console.log(`Trying to fetch ${event.request.url}`);
+    event.respondWith(checkCache(event.request));
+});
 
 async function checkCache(req) {
     const cachedResponse = await caches.match(req);
