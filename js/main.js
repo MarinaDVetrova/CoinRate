ef6e9cd745e995b5ff0b73ac3b83532fb62b2e9a
MarinaDVetrova <33320338+MarinaDVetrova@users.noreply.github.com>
2021-04-16 18:23:48 +0300

    CR

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const addCurrencyBtn = document.querySelector(".add-currency-btn");
 const addCurrencyList = document.querySelector(".add-currency-list");
 const currenciesList = document.querySelector(".currencies");
 
-const dataURL = "https://coin-rate.xyz/latest.json";
+const dataURL = "http://api.exchangeratesapi.io/v1/latest?access_key=d204c5aa1d1b58d37e85e200a29eb6cc";
 
 const initiallyDisplayedCurrencies = ["q","BTC", "RUB", "USD", "EUR", "GBP", "JPY"];
 let baseCurrency;
